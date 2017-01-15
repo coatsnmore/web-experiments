eb60514c1208f2efccf0d20a4fbbf15555f66f81
coatsnmore <coats.18@gmail.com>
2017-01-15 16:08:35 -0500

    added synth; connected synth playSong to button on DOM;

---
+++ es6/Synth.js
@@ -0,0 +1,76 @@
+import Notes from './Notes.js';
+
+class Synth {
+
+    constructor() {
+        this.notes = Notes.all();
+        // var context = {};
+        var ContextClass = (window.AudioContext ||
+            window.webkitAudioContext ||
+            window.mozAudioContext ||
+            window.oAudioContext ||
+            window.msAudioContext),
+            buffer, thisSynth = this;
+        if (ContextClass) {
+            // Web Audio API is available.
+            this.context = new ContextClass();
+        } else {
+            console.error('Browser does not support AudioContext, uh oh!');
+        }
+    }
+
+    playSong() {
+        var duration = 0.5,
+            startTime = this.context.currentTime,
+            song = ['G3', 'A#3/Bb3', 'D#4/Eb4', 'G3', 'A#3/Bb3', 'G#3/Ab3', 'G3'],
+            // song = ['G3', 'G3', 'G3', 'G3', 'G3', 'G#3/Ab3', 'G3'],
+            oscillatorType = 'sine',
+            masterVolume = 0.1;
+
+
+        for (var i = 0; i < song.length; i++) {
+            // controller.updateSpheresByNote(song[i]);
+            this.play(song[i], startTime + duration * i, duration, oscillatorType, masterVolume);
+        }
+    }
+
+    play(note, startTime, duration, type, volumeLevel) {
+        var osc1 = this.context.createOscillator(),
+            osc2 = this.context.createOscillator(),
+            volume = this.context.createGain(),
+            frequency = this.notes[note];
+
+        volume.gain.value = volumeLevel;
+
+        // Set oscillator wave type
+        osc1.type = type;
+        osc2.type = type;
+
+        // tune
+        osc1.frequency.value = frequency;
+        osc2.frequency.value = frequency - 3;
+
+        // wire em up
+        // osc1.connect(analyser);
+        // analyser.connect(freqAnalyser);
+        // freqAnalyser.connect(volume);
+        osc1.connect(volume);
+
+        osc2.connect(volume);
+        volume.connect(this.context.destination);
+
+        // Fade out
+        volume.gain.setValueAtTime(0.1, startTime + duration - 0.25);
+        volume.gain.linearRampToValueAtTime(0, startTime + duration);
+
+        // Start oscillators
+        osc1.start(startTime);
+        osc2.start(startTime);
+
+        // Stop oscillators
+        osc1.stop(startTime + duration);
+        osc2.stop(startTime + duration);
+    }
+}
+
+export default Synth;
--- es6/main.js
+++ es6/main.js
@@ -1,12 +1,15 @@
 import EventNotification from './EventNotification.js';
 import Scene from './Scene.js';
+import Synth from './Synth.js';
 
+// wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function () {
 
     let width = document.documentElement.clientWidth;
     let height = document.documentElement.clientHeight;
     let eventNotification = new EventNotification();
     let scene = new Scene('scene', width, 300);
+    let synth = new Synth();
 
     // start this thang!
     scene.tick();
@@ -14,6 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // connect bundled stuff to DOM controller
     Controller.notify = function () {
         eventNotification.meow();
+    };
+
+    Controller.playSong = function () {
+        synth.playSong();
     };
 });
 
