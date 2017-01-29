b14a0d223cc5f5fb48a4ba275403e9b36ef8b44b
coatsnmore <coats.18@gmail.com>
2017-01-29 18:55:17 -0500

    added home image

--- es6/components/Home.vue
+++ es6/components/Home.vue
@@ -1,9 +1,13 @@
 <template>
 	<div>
-		<h2>Welcome to the <em>coatsn</em> laboratory.</h2>
-		<p>There are many experiments waiting for those who dare to explore the modern web!</p>
-		<p>Beware, though. For those who enter are never heard from again!</p>
-	</div>
+		<div class="six columns">
+			<h2>Welcome to the <em>coatsn</em> laboratory.</h2>
+			<p>There are many experiments waiting for those who dare to explore the modern web!</p>
+			<p>Beware, though. For those who enter are never heard from again!</p>
+		</div>
+		<div class="six columns">
+			<img src="img/cat-medium.jpg">
+		</div>
 </template>
 
 <script>
